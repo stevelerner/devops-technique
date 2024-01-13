a35a941038db8485229badc06d584c6252800b00
Steve Lerner <7978887+stevelerner@users.noreply.github.com>
2024-01-13 12:33:42 -0500

    Update aws-run.sh

--- aws-cli/aws-run.sh
+++ aws-cli/aws-run.sh
@@ -1,5 +1,10 @@
 aws ec2 run-instances \
-    --image-id ami-0557a15b87f6559cf \
-    --instance-type t3.large \
-    --security-group-ids sg-03dd9bca624edbbb8 \
-    --key-name infraarc-demo
+--image-id ami-0e83be366243f524a \
+--count 1 \
+--instance-type t2.xlarge \
+--key-name stevelernercxuseast2 \
+--security-group-ids sg-0b255a03981b65b40 \
+--subnet-id subnet-0517e5c4de64acc2b \
+--associate-public-ip-address \
+--tag-specifications \
+'ResourceType=instance, Tags=[{Key=user,Value=steve.lerner},{Key=Name,Value=slerner-2xl}]'
