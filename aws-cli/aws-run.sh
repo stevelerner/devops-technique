2a67fb338429f2ac8ee7305fde5ae6eed4688e7a
Steve Lerner <7978887+stevelerner@users.noreply.github.com>
2024-03-07 14:05:50 -0500

    Update aws-run.sh

--- aws-cli/aws-run.sh
+++ aws-cli/aws-run.sh
@@ -6,5 +6,6 @@ aws ec2 run-instances \
 --security-group-ids sg-0b255a03981b65b40 \
 --subnet-id subnet-0517e5c4de64acc2b \
 --associate-public-ip-address \
---block-device-mappings '[{"DeviceName":"/dev/sda1","Ebs":{"VolumeSize":48,"DeleteOnTermination":true}}]' \
---tag-specifications 'ResourceType=instance,Tags=[{Key=user,Value=steve.lerner},{Key=Name,Value=slerner-2xl}]'
+--block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=50}' \
+--tag-specifications \
+'ResourceType=instance, Tags=[{Key=user,Value=steve.lerner},{Key=Name,Value=slerner-2xl-32GB}]' 'ResourceType=volume, Tags=[{Key=Name,Value=YourVolumeName}]'`
