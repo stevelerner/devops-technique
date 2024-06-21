0784ea6d0e0d288d636a6e6298adc5ef24e46e2e
Steve Lerner <7978887+stevelerner@users.noreply.github.com>
2024-06-21 10:44:14 -0400

    Update aws-ecr-login.sh

--- aws-cli/aws-ecr-login.sh
+++ aws-cli/aws-ecr-login.sh
@@ -1,1 +1,2 @@
 aws ecr-public get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin public.ecr.aws
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 104013952213.dkr.ecr.us-east-1.amazonaws.com
